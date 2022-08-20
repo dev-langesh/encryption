0eb74ffd278f64bba0b8637ce8212cbba5408305
dev-langesh <dev.langesh@gmail.com>
2022-08-20 17:34:08 +0530

    imporved modularity

--- decrypt.js
+++ decrypt.js
@@ -1,65 +1,11 @@
-const C = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  " ",
-];
+const { set } = require("./resource");
 
 function decrypt(cipher, key) {
   const decrypted = cipher.split("").map((c) => {
-    const index = C.findIndex((i) => i === c);
+    const index = set.findIndex((i) => i === c);
     let value = (index - key) % 53;
     if (value < 0) value = 53 + value;
-    const plain = C[value];
+    const plain = set[value];
     return plain;
   });
 
--- encrypt.js
+++ encrypt.js
@@ -1,64 +1,10 @@
-const C = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  " ",
-];
+const { set } = require("./resource");
 
 function encrypt(plainText, key) {
   const plainArr = plainText.split("");
   const encrypted = plainArr.map((p) => {
-    const index = C.findIndex((l) => l === p);
-    return C[(index + key) % 53];
+    const index = set.findIndex((l) => l === p);
+    return set[(index + key) % 53];
   });
   return encrypted.join("");
 }
